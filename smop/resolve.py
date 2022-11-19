9a3f3dde7be92df016f33df33c9ea26011cc8916
Bill Stark <billstark001@gmail.com>
2022-11-19 18:20:17 +0900

    Update resolve.py

--- smop/resolve.py
+++ smop/resolve.py
@@ -50,7 +50,7 @@ def resolve(t, symtab=None, fp=None, func_name=None):
     do_resolve(t, symtab)
     G = as_networkx(t)
     for n in G.nodes():
-        print(n.__class__.__name__)
+        
         u = G.nodes[n]["ident"]
         if u.props:
             pass
